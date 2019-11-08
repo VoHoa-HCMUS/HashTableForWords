b78595988a58399edfdd78b87c5470e4b1984caf
VoHoa <55783353+VoHoa-HCMUS@users.noreply.github.com>
2019-11-08 12:20:58 +0700

    Add project files.

---
+++ HashTable/HashTable.cpp
@@ -0,0 +1,120 @@
+#include "HashTable.h"
+#define prime 399989
+#define prime2 372637
+int EncodeString(string s){
+	int code = 0;
+	for (int i = 0; i < s.length(); i++) {
+		code += toascii(s[i]) * (i + 1);
+	}
+	return code;
+}
+void LinearInsert(string s, string* HashTable) {
+    int k = HashFunction(s);
+	int i = 0;
+	while (HashTable[k] != "") {
+		k = (k + i) % prime;
+		i++;
+	}
+	HashTable[k] = s;
+	cout << "Hashed " << HashTable[k] << " in " << k << endl;
+}
+int HashFunction(string s){
+	return EncodeString(s) % prime;
+}
+void OutPutPosHashed(string* HasTable,int n) {
+	for (int i = 0; i < n; i++) {
+		if (HasTable[i] != "")
+			cout << "Not empty!!!: " <<i<<" "<< HasTable[i]<< endl;
+	}
+}
+bool LinearSearch(string s, string* HashTable) {
+	int k = HashFunction(s);
+	int i = 0;
+	while (HashTable[k] != s && HashTable[k] != "") {
+		k = (k + i) % prime;
+		i++;
+	}
+	if (HashTable[k] == s)
+		return true;
+	return false;
+}
+void SeperateChaining(string s, List*& HashTable){
+	int k = HashFunction(s);
+	AddHead(HashTable[k], s);
+
+}
+bool SearchSeperateChaining(string s, List* HashTable){
+	int k = HashFunction(s);
+	if (HashTable[k].pHead == NULL)
+		return false;
+	for (Node* p = HashTable[k].pHead; p; p = p->pNext)
+	{
+		if (p->data == s)
+			return true;
+	}
+	return false;
+}
+void AddHead(List& l, string s) {
+	Node* p = new Node;
+	p->data = s;
+	p->pNext = NULL;
+	if (l.pHead == NULL)
+		l.pHead = p;
+	else
+	{
+		p->pNext = l.pHead;
+		l.pHead = p;
+	}
+}
+void AllocateList(List*& l, int n){
+	for (int i = 0; i < n; i++)
+		l[i].pHead = NULL;
+}
+void QuadraticProbing(string s, string* HashTable) {
+	int k = HashFunction(s);
+	int i=0;
+	while (HashTable[k] != ""){
+		k = (k + i*i) % prime;
+		i++;
+	}
+	HashTable[k] = s;
+	cout << "Hashed " << HashTable[k] << " in " << k << endl;
+}
+bool QuadraticSearch(string s, string* HashTable) {
+	int k = HashFunction(s);
+	int i = 0;
+	while (HashTable[k] != s && HashTable[k] != "") {
+		k = (k + i*i) % prime;
+		i++;
+	}
+	if (HashTable[k] == s)
+		return true;
+	return false;
+}
+int Hash2Function(string s) {
+	return prime2+EncodeString(s)%prime2;
+}
+void DoubleHash(string s, string* HashTable) {
+	int k1 = HashFunction(s), k2 = Hash2Function(s);
+	int i = 0;
+	int k = (k1 + i * k2) % prime;
+	while (HashTable[k] != "") {
+	    k = (k1 + i * k2) % prime;
+		i++;
+	}
+	HashTable[k] = s;
+	cout << "Hashed " << HashTable[k] << " in " << k << endl;
+
+}
+bool DoubleHashSearch(string s, string* HashTable) {
+	int k1 = HashFunction(s), k2 = Hash2Function(s);
+	int i = 0;
+	int k = (k1 + i * k2) % prime;
+	while (HashTable[k] != "") {
+		k = (k1 + i * k2) % prime;
+		i++;
+	}
+	if (HashTable[k] == s)
+		return true;
+	return false;
+}
---
+++ HashTable/main.cpp
@@ -0,0 +1,149 @@
+#include"HashTable.h"
+#include<fstream>
+#include<string>
+#define MAX 400000
+int main(){
+	ifstream filein;
+	filein.open("words.txt");
+	string input;
+	cout << "Chon chien luoc giai quyet dung do:" << endl;
+	cout << "1. Noi ket" << endl;
+	cout << "2. Do tuyen tinh" << endl;
+	cout << "3. Do bac hai" << endl;
+	cout << "4. Bam kep" << endl;
+	int op;
+	cin >> op;
+	switch (op)
+	{
+	case 1:
+	{
+		int num = 0;
+		List* HashTable = new List[MAX];
+		AllocateList(HashTable, MAX);
+		cout << "Hashing please wait..." << endl;
+		while (!filein.eof()) {
+			if (filein.tellg() == -1) {
+				break;
+			}
+			string temp;
+			filein >> temp;
+			SeperateChaining(temp, HashTable);
+		}
+		filein.close();
+		while (true) {
+			int op1;
+			cout << "Nhap tu can tim:";  
+			cin.clear();
+			cin.ignore();
+			getline(cin, input);
+			if (SearchSeperateChaining(input, HashTable))
+				cout << input << " La Tu Tieng Anh" << endl;
+			else
+				cout << input << " Ko La Tu Tieng Anh" << endl;
+			cout << "Nhap 1 de tiep tuc tim, 0 de thoat:";
+			cin.clear();
+			cin >> op1;
+			if (op1 == 0)
+				break;
+		}
+		delete[]HashTable;
+	}
+	break;
+	case 2:
+	{
+		string* HashTable = new string[MAX];
+		int num=0;
+		while (!filein.eof()) {
+			if (filein.tellg() == -1) {
+				break;
+			}
+			string temp;
+			filein >> temp;
+			LinearInsert(temp, HashTable);
+		}
+		filein.close();
+		while (true) {
+			cout << "Nhap tu can tim:";
+			cin.clear();
+			cin.ignore();
+			getline(cin, input);
+			if (LinearSearch(input, HashTable))
+				cout << input << " La Tu Tieng Anh" << endl;
+			else
+				cout << input << " Ko La Tu Tieng Anh" << endl;
+			int op1;
+			cout << "Nhap 1 de tiep tuc tim, 0 de thoat:";
+			cin.clear();
+			cin >> op1;
+			if (op1 == 0)
+				break;
+		}
+		delete[]HashTable;
+	}
+	break;
+	case 3:
+	{
+		string* HashTable = new string[MAX];
+		while (!filein.eof()) {
+			if (filein.tellg() == -1) {
+				break;
+			}
+			string temp;
+			filein >> temp;
+			QuadraticProbing(temp, HashTable);
+		
+		}
+		filein.close();
+		while (true) {
+			cout << "Nhap tu can tim:";
+			cin.clear();
+			cin.ignore();
+			getline(cin, input);
+			if (QuadraticSearch(input, HashTable))
+				cout << input << " La Tu Tieng Anh" << endl;
+			else
+				cout << input << " Ko La Tu Tieng Anh" << endl;
+			int op1;
+			cout << "Nhap 1 de tiep tuc tim, 0 de thoat:";
+			cin >> op1;
+			if (op1 == 0)
+				break;
+		}
+		delete[]HashTable;
+	}
+	break;
+	case 4:
+	{
+		string* HashTable = new string[MAX];
+		int num = 0;
+		while (!filein.eof()) {
+			if (filein.tellg() == -1) {
+				break;
+			}
+			string temp;
+			filein >> temp;
+			DoubleHash(temp, HashTable);
+		}
+		filein.close();
+		while (true) {
+			cout << "Nhap tu can tim:";
+			cin.clear();
+			cin.ignore();
+			getline(cin, input);
+			if (DoubleHashSearch(input, HashTable))
+				cout << input << " La Tu Tieng Anh" << endl;
+			else
+				cout << input << " Ko La Tu Tieng Anh" << endl;
+			int op1;
+			cout << "Nhap 1 de tiep tuc tim, 0 de thoat:";
+			cin >> op1;
+			if (op1 == 0)
+				break;
+		}
+		delete[]HashTable;
+	}
+	default:
+		break;
+	}
+	return 0;
+}
