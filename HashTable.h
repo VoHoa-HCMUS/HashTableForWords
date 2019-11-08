ec91851e196d875ee98eca3c47b08884a7aad37f
VoHoa <55783353+VoHoa-HCMUS@users.noreply.github.com>
2019-11-08 12:34:06 +0700

    Add files via upload
    fix function name

---
+++ HashTable.h
@@ -0,0 +1,25 @@
+#pragma once
+#include<iostream>
+#include<list>
+using namespace std;
+struct Node{
+	string data;
+	Node* pNext;
+};
+struct List{
+	Node* pHead;
+};
+void AddHead(List&l,string s);
+int EncodeString(string s);
+void LinearProbing(string s, string* HashTable);
+int HashFunction(string s);
+int Hash2Function(string s);
+bool LinearSearch(string s, string* HashTable);
+void SeperateChaining(string s, List*&HashTable);
+void AllocateList(List*&l, int n);
+bool SearchSeperateChaining(string s, List* HashTable);
+void QuadraticProbing(string s, string* HashTable);
+bool QuadraticSearch(string s, string* HashTable);
+void DoubleHash(string s, string* HashTable);
+bool DoubleHashSearch(string s, string* HashTable);
+
